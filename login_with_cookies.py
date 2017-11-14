7b7fd130ad34c73fb6c92cefa1e2da0e1c55f721
Michael Guo <1582189677@qq.com>
2017-11-14 11:06:49 +0800

    增加获取指定的一组域名的cookies;增加更新cookies功能

--- login_with_cookies.py
+++ login_with_cookies.py
@@ -3,6 +3,7 @@ import requests
 import os
 import sqlite3
 from win32crypt import CryptUnprotectData
+from collection import namedtuple
 
 # cookies存放路径
 cookie_path = os.environ['LOCALAPPDATA'] + r"\Google\Chrome\User Data\Default\Cookies"
@@ -16,6 +17,25 @@ def get_cookie_from_chrome(host=None):
                      for host_key, name, encrypted_value in cursor.execute(sql).fetchall() }
         return cookies
 
+# 获取指定一组域名的cookies
+cookies_domain = ['.youdao.com', '.note.youdao.com', 'note.youdao.com'] # example
+def get_all_cookies(domains=None):
+    cookies = {}
+    for domain in cookies_domain:
+        cookies.update(get_cookie_from_chrome(domain))
+    return cookies
+all_domain_cookies = get_all_cookies(cookies_domain) # 获取指定域名的cookies
+
+# 更新指定cookies
+def update_cookies(new_cookies, old_cookies):
+    flash_cookies = {}
+    for key in old_cookies.keys():
+        flash_cookies[key] = new_cookies[key] if key in new_cookies.keys() else old_cookies[key]
+    return flash_cookies
+
+# flash_cookies = update_cookies(new_cookies=all_domain_cookies, old_cookies=format_cookies )
+# old_cookies 接收字典，主要包含需要的字段名，以及一些预备值，以防新cookies中没有某些值
+    
 # 获取所有hosts
 def get_hosts():
     sql = "select host_key from cookies"
@@ -33,6 +53,7 @@ test_url = 'http://i.baidu.com'                         # 设定测试url
 test_host = '.baidu.com'                                # 设定测试host
 test_cookies = get_cookie_from_chrome(test_host)        # 获取测试host的cookies
 user_name = "那******晴"
+
 
 if __name__ == "__main__" :
     # 使用get请求访问测试url
