96706121f896b9a463de6307139c423af6e4616d
Michael Guo <1582189677@qq.com>
2018-03-12 21:23:09 +0800

    统计ip地址频率

---
+++ count_ip.py
@@ -0,0 +1,16 @@
+import re
+
+# 匹配ip地址
+reg = re.compile(r'(\d{1,3}\.}){3}(\d{1,3})')
+
+# 统计ip次数
+ip_seq = {}
+
+with open('/var/log/secure') as e:
+    f = e.readline()
+    for i in f:
+        t = re.search(reg, i)
+        if t:
+            ip_key = t.group(0)
+            ip_seq[ip_key] = ip_seq.get(ip_key, 0) + 1
+print(ip_seq)
